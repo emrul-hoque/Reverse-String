4c20afa9443ef0e5b12474835d9a1c189cb41c73
Emrul <emrul.hoque@gmail.com>
2023-03-22 20:17:09 +0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Please type a word to reverse it");
+        string? str = Console.ReadLine();
+        if (str != null)
+        {
+        string revStr = new string(str.Reverse().ToArray());
+        Console.WriteLine("The reveresed word is " + revStr);
+        }
+        else
+        {
+            Console.WriteLine("You have not entered a word");
+        }
+
+    }
+}
